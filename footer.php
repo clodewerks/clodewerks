c6daf40534a29e13bbe684070706720d00298079
Kyle Linneman <clodewerks@gmail.com>
2016-01-27 20:50:08 -0800

    Adding link to Git

--- footer.php
+++ footer.php
@@ -3,7 +3,8 @@
 	<a href="mailto:clodewerks@gmail.com"><i class="fa fa-envelope-o"></i></a><div class="footDash">-</div>
 	 <a href="https://www.facebook.com/pages/Clodewerks/175609938953"><i class="fa fa-facebook-square"></i></a><div class="footDash">-</div>
 	   <a href="http://soundcloud.com/clodewerks"><i class="fa fa-soundcloud"></i></a><div class="footDash">-</div>
-	   <a href="https://www.linkedin.com/in/kyle-linneman-407b6a22"><i class="fa fa-linkedin-square"></i></a>
+	   <a href="https://www.linkedin.com/in/kyle-linneman-407b6a22"><i class="fa fa-linkedin-square"></i></a><div class="footDash">-</div>
+	   <a href="https://github.com/clodewerks/"><i class="fa fa-github"></i></a>
 </div>	
 	</body>
 </html>
