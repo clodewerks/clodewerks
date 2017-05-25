682f83bfa4871d419252dcac33989683a1c98019
Kyle Linneman <clodewerks@gmail.com>
2017-05-24 19:59:30 -0700

    Fixing resume link, adding github

--- js/mobiletest.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function ()
-	if ((navigator.userAgent.indexOf('iPhone') != -1) || (navigator.userAgent.indexOf('iPod') != -1) || (navigator.userAgent.indexOf('iPad') != -1)) {
-		$(".headdescription").css("display","inline");
-		
-	} 
-	else {
-		alert("ha");
-	}
-);
